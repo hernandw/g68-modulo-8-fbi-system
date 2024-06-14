1e665184ed299dd78b3c4063867bcab1c5cf5d80
Williams Hernández <hernandw@gmail.com>
2024-06-14 19:09:30 -0400

    :sparkles: formularios de login y registro

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,27 @@ export const home = (req, res) => {
     title: "Home Page",
   });
 };
+
+export const loginForm = (req, res) => {
+  res.render("login", {
+    title: "Login Page",
+  });
+};
+
+export const registerForm = (req, res) => {
+  res.render("register", {
+    title: "Register Page",
+  });
+};
+
+export const contactForm = (req, res) => {
+  res.render("contact", {
+    title: "Contact Page",
+  });
+};
+
+export const admin = (req, res) => {
+  res.render("admin", {
+    title: "Admin Page",
+  });
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { home } from '../controllers/userController.js'
+import { home, loginForm, registerForm, contactForm, admin } from '../controllers/userController.js'
 const router = express.Router()
 
 router.get('/', home)
+
+router.get('/login', loginForm)
+router.get('/register', registerForm)
+router.get('/contact', contactForm)
+router.get('/admin', admin)
 
 router.get('*', (req, res)=>{
 res.send('404 - page not found')
