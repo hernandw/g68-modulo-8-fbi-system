f3898949594955d617d6eb969a99aaf9dfbb9757
Williams Hernández <hernandw@gmail.com>
2024-06-21 22:03:36 -0400

    Cambios

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
-import pg from 'pg';
-import 'dotenv/config';
+import pg from "pg";
+import "dotenv/config";
 const { Pool } = pg;
 
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const config = {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    allowExitOnIdle: true,
-    ssl: false
-}
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  allowExitOnIdle: true,
+  ssl: { rejectUnauthorized: false },
+};
 
-
-export const pool = new Pool(config)
+export const pool = new Pool(config);
